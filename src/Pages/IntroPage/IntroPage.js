903ee546ae119a06cee8a4ce708d76ae40cee7b0
tubidaor <juan.baltazar1@gmail.com>
2020-09-20 16:57:29 -0700

    finished homepage styling

--- src/Pages/IntroPage/IntroPage.js
+++ src/Pages/IntroPage/IntroPage.js
@@ -4,22 +4,39 @@ import './IntroPage.css'
 import config from '../../config'
 
 export default class IntroPage extends Component {
+  defaultProps = {
+    location: {},
+    history: {
+      push: () => {}
+    }
+  }
+
+  handleLoginPath = () => {
+    const { history } = this.props
+    history.push('/login')
+  }
+  
+  handleRegPath = () => {
+    const { history } = this.props
+    history.push('/register')
+  }
   render() {
 
     return (
       <div className="introPageContainer">
-        <h1 className="introH1">Te Quiero Mucho</h1>
+        <div className="introPageTop">
+        <h2 className="introH1">Te Quiero Mucho</h2>
         <img className="introImg" alt="logo for app" src={`${config.API_ENDPOINT}/uploads/static/treeOfLife2.png`}/>
-        <p className="introP">We would like to think that all you need is love. However, a relationship can only grow if two people are committed to each other and the relationship.</p>
-        <div className="introButtonContainer">
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="register">
-            <button>Register</button>
-          </Link>
+        <p className="introP">Relationships need cultivating: we're here to help.</p>
         </div>
-        <div className="introLighting"></div>
+        <div className="introButtonContainer">
+          <button onClick={this.handleLoginPath}>
+              Login
+          </button>
+          <button onClick={this.handleRegPath}>
+              Register
+          </button>
+        </div>
       </div>
     )
   }
